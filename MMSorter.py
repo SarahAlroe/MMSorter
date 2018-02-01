cf3f3a055e09768ec7ce24f43e65c511602528d1
Silas Fjelsted Alrøe <silasalroe@gmail.com>
2018-02-01 01:01:04 +0100

    More general progress

--- MMSorter.py
+++ MMSorter.py
@@ -1,7 +1,80 @@
 # import the necessary packages
 import numpy as np
-import argparse
 import cv2
+import serial
+from enum import Enum
+
+class AppState(Enum):
+    IDLE = 1
+    MOVE = 2
+    ACTIVE = 3
+
+#
+# Global consts
+#
+serialTimeout = 1
+baudrate = 9600
+
+screenWidth = 1920
+screenHeight = 1080
+screenImageProportion = 0.8
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+idletext = "Mid: Sort \nUp: Home \nDown: Move \nLeft: Set color \nRight: Calibrate"
+runningText = "Currently running! \nPress middle button to stop"
+moveText = "Move mode. \nUse buttons to move head. \nMiddle button to confirm position"
+
+# Clean button colors
+pRed = (0, 0, 255)
+pGreen = (0, 255, 0)
+pBlue = (255, 0, 0)
+pYellow = (0, 255, 255)
+pOrange = (0, 165, 255)
+pBrown = (42, 42, 165)
+pColors = [pRed, pGreen, pBlue, pYellow, pOrange, pBrown]
+
+# Experimentally chosen values
+iRed = (62, 46, 105)
+iGreen = (52, 87, 47)
+iBlue = (53, 36, 21)
+iYellow = (70, 146, 155)
+iOrange = (70, 102, 151)
+iBrown = (25, 30, 39)
+iColors = [iRed, iGreen, iBlue, iYellow, iOrange, iBrown]
+
+#
+# Global vars
+#
+sorterStatus = "Awaiting connection"
+currentState = AppState.IDLE
+isOk = False
+cPos = [0, 0]
+buttonClicked = [False, False, False, False, False]
+
+
+#
+# Function defs
+#
+
+def handleSerial():
+    while ser.in_waiting!=0:
+        line = ser.readline()
+        print line
+
+        if line.startswith("OK"):
+            isOk = True
+        elif line.startswith("BP"):
+            buttonNumber = int(line[2])
+            buttonClicked[buttonNumber] = True
+        elif line.startswith("BR"):
+            buttonNumber = int(line[2])
+            buttonClicked[buttonNumber] = False
+        elif line.startswith("CP"):
+            cPos[0] = int(line[2:6])
+            cPos[1] = int(line[6:10])
+        elif line.startswith("PS"):
+            sorterStatus = line[2:]
+
 
 def findPoints(image):
     # Make gray slightly blurred image for optimal circle detection
@@ -17,6 +90,8 @@ def findPoints(image):
     return detectedcircles
 
 def getNewImage():
+    # load the image, clone it for output, and then convert it to grayscale
+    # image = cv2.imread(args["image"])
     return cv2.imread("testimg.jpg")
 
 def drawWindowContent(outputScaled, outputMenu):
@@ -26,7 +101,7 @@ def drawCirclesOnimage(image, circles):
     # For each circle as their xypos and radius
     for (x, y, r) in circles:
         # Calculate mean color of circle
-        circleImg = np.zeros((height, width), dtype=image.dtype)
+        circleImg = np.zeros((image.shape[0], image.shape[1]), dtype=image.dtype)
         cv2.circle(circleImg, (x, y), r, 1, -1)
         meanVal = cv2.mean(image, mask=circleImg)
 
@@ -50,45 +125,73 @@ def drawCirclesOnimage(image, circles):
 
     return image
 
-screenWidth = 1920
-screenHeight = 1080
-screenImageProportion = 0.8
 
-# Clean button colors
-pRed = (0, 0, 255)
-pGreen = (0, 255, 0)
-pBlue = (255, 0, 0)
-pYellow = (0, 255, 255)
-pOrange = (0, 165, 255)
-pBrown = (42, 42, 165)
-pColors = [pRed, pGreen, pBlue, pYellow, pOrange, pBrown]
+def draw_menu_button(image, pos, state):
+    r = 20
+    if state:
+        cv2.circle(image, pos, r, (255, 255, 255), -1)
+    else:
+        cv2.circle(image, pos, r, (255, 255, 255), 3)
 
-# Experimentally chosen values
-iRed = (62, 46, 105)
-# iRed = (90, 46, 105) #Artificial
-iGreen = (52, 87, 47)
-iBlue = (53, 36, 21)
-iYellow = (70, 146, 155)
-iOrange = (70, 102, 151)
-iBrown = (25, 30, 39)
-# iBrown = (15, 25, 26)#Artificially lowered
-iColors = [iRed, iGreen, iBlue, iYellow, iOrange, iBrown]
 
-# construct the argument parser and parse the arguments
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-i", "--image", required=True, help="Path to the image")
-# args = vars(ap.parse_args())
+def generate_menu_image_from_shape(shape):
+    width = int(screenHeight * (1 - screenImageProportion))
+    height = int(screenHeight * screenImageProportion / shape[0] * shape[1])
+
+    menu = np.zeros((width, height, 3), np.uint8)
+    print width
+    cv2.putText(menu, sorterStatus, (5, width-10), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(menu, str(cPos), (height-150, width - 10), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+    butttonCenterX = height-200
+    buttonCenterY = 80
+    buttonOffset = 45
+
+    draw_menu_button(menu, (butttonCenterX,buttonCenterY), buttonClicked[0])
+    draw_menu_button(menu, (butttonCenterX, buttonCenterY-buttonOffset), buttonClicked[1])
+    draw_menu_button(menu, (butttonCenterX, buttonCenterY+buttonOffset), buttonClicked[2])
+    draw_menu_button(menu, (butttonCenterX-buttonOffset, buttonCenterY), buttonClicked[3])
+    draw_menu_button(menu, (butttonCenterX+buttonOffset, buttonCenterY), buttonClicked[4])
+
+    y0 = 30
+    dy = 25
+    if currentState == AppState.IDLE:
+        for i, line in enumerate(idletext.split('\n')):
+            y = y0 + i * dy
+            cv2.putText(menu, line, (5, y), font, 0.75, (255, 255, 255), 1, cv2.LINE_AA)
+    elif currentState == AppState.MOVE:
+        for i, line in enumerate(moveText.split('\n')):
+            y = y0 + i * dy
+            cv2.putText(menu, line, (5, y), font, 0.75, (255, 255, 255), 1, cv2.LINE_AA)
+    elif currentState == AppState.ACTIVE:
+        for i, line in enumerate(runningText.split('\n')):
+            y = y0 + i * dy
+            cv2.putText(menu, line, (5, y), font, 0.75, (255, 255, 255), 1, cv2.LINE_AA)
+
+    return menu
+
+
+def resize_img_for_screen(originalImage):
+    return cv2.resize(originalImage, (int(screenHeight * screenImageProportion / originalImage.shape[0] * originalImage.shape[1]),
+                               int(screenHeight * screenImageProportion)))
+
+
+#
+# Main
+#
+#Setup serial
+#ser = serial.Serial('/dev/ttyUSB0', baudrate, timeout=serialTimeout)
+ser = serial.Serial('/dev/ttyACM0', baudrate, timeout=serialTimeout)
 
 #Setup window
 cv2.namedWindow("MMSorter", cv2.WINDOW_NORMAL)
 cv2.setWindowProperty("MMSorter", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-# load the image, clone it for output, and then convert it to grayscale
-# image = cv2.imread(args["image"])
+#Get image
 image = getNewImage()
+
 output = image.copy()
 # Get image dimensions
-height, width, depth = image.shape
 
 circles = findPoints(image)
 
@@ -98,13 +201,17 @@ if circles is not None:
     cv2.imwrite("output2.jpg", output)
 
     # Show the output image
-    outputScaled = cv2.resize(output, (int(screenHeight * screenImageProportion / output.shape[0] * output.shape[1]),
-                                       int(screenHeight * screenImageProportion)))
-    outputMenu = np.zeros((int(screenHeight * (1 - screenImageProportion)),
-                           int(screenHeight * screenImageProportion / output.shape[0] * output.shape[1]), 3), np.uint8)
+    outputScaled = resize_img_for_screen(output)
+    outputMenu = generate_menu_image_from_shape(output.shape)
     drawWindowContent(outputScaled, outputMenu)
     #  Wait for a few clicks
     cv2.waitKey(0)
+    handleSerial()
+    outputMenu = generate_menu_image_from_shape(output.shape)
+    drawWindowContent(outputScaled, outputMenu)
     cv2.waitKey(0)
+    handleSerial()
+    outputMenu = generate_menu_image_from_shape(output.shape)
+    drawWindowContent(outputScaled, outputMenu)
     cv2.waitKey(0)
 
